01f14ebc09013243bafbf0ddd543457f178606db
simomy8kolanenko <my8kola.simonenko@gmail.com>
2025-02-03 15:17:49 -0500

    index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+   <?php require __DIR__ . '/filter.php' ?>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
